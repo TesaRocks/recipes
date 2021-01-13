bf79a963cc10aba657d1ed1086c5dccde29e6a9e
Alexis Dietl <alexisdietl@gmail.com>
2021-01-13 17:01:56 -0300

    chapter 22 lesson 328 Auth Module creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipe/recipes.module';
 import { ShoppingModule } from './shopping/shopping.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingModule,
     SharedModule,
     CoreModule,
+    AuthModule,
   ],
   bootstrap: [AppComponent],
 })
